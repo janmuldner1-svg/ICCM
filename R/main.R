094b3745ff20d19c1af90990a7b204881496a386
Tosca Koeze <tosca.koeze@wur.nl>
2025-10-06 16:32:09 +0200

    attempt 2: sourced and called functions to download official concession data

--- R/main.R
+++ R/main.R
@@ -5,3 +5,12 @@
 if(!dir.exists("tempfiles")){dir.create("tempfiles")}
 if(!dir.exists("data")){dir.create("data")}
 if(!dir.exists("output")){dir.create("output")}
+
+# Source the download function
+source("downloadFiles.R")
+
+# Download the official timber and oil palm concession data
+download_timber_concessions()
+download_oil_palm_concessions()
+
+
