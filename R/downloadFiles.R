b3787d345c9e249b97f75051c67446a95c088ebf
Jan Müldner <jan.muldner@wur.nl>
2025-10-07 16:15:44 +0200

    added function that downloads adm lvl 2 for Indonesia and extracts Kayong Utara regency as project ROI

--- R/downloadFiles.R
+++ R/downloadFiles.R
@@ -1,3 +1,36 @@
+## File for downloading necessary datasets
+
+# Packages
+library(sf)
+library(dplyr)
+
+# Download the boundaries of Kayong Utara regency as extent (project ROI)
+download_and_extract_Kayong <- function() {
+  # Define URL and destination path
+  adm2_URL <- 'https://github.com/wmgeolab/geoBoundaries/raw/9469f09/releaseData/gbOpen/IDN/ADM2/geoBoundaries-IDN-ADM2_simplified.geojson'
+  dest_folder <- "data"
+  destfile <- file.path(dest_folder, "geoBoundaries-IDN-ADM2_simplified.geojson")
+  outputfile <- file.path(dest_folder, "Kayong_boundary.geojson")
+  
+  if (!dir.exists(dest_folder)) dir.create(dest_folder)
+  if (!file.exists(destfile)) download.file(adm2_URL, destfile, mode = "wb")
+  
+  # Load full GeoJSON into R as sf object
+  adm2 <- sf::st_read(destfile, quiet = TRUE)
+  
+  # Filter dataset to only keep row with Kayong Utara
+  kayong <- dplyr::filter(adm2, shapeName == "Kayong Utara")
+  
+  # Stop code if there is no Kayong Utara row (to prevent it from breaking)
+  if (nrow(kayong) == 0) return(NULL)
+  
+  # Save Kayong boundary as new .geojson
+  sf::st_write(kayong, outputfile, delete_dsn = TRUE, quiet = TRUE)
+  # Remove the original file with all regencies, return Kayong boundary
+  file.remove(destfile)
+  return(kayong)
+}
+
 # Download the timber concession data
 download_timber_concessions <- function(){
   # Download managed forest concessions
@@ -26,5 +59,7 @@ download_oil_palm_concessions <- function(){
   }
 }
 
+# Forest loss dataset in MS Teams
+# Oil palm dataset? 
 
 
