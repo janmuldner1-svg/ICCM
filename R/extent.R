163f26726224d72a61759fc55771fe503fab8bbd
Joellev3 <joelle.vandrie@wur.nl>
2025-10-07 14:58:06 +0200

    made start on function createExtent to convert crop the data to the right extent

---
+++ R/extent.R
@@ -0,0 +1,21 @@
+if(!"sf" %in% installed.packages()){install.packages("sf")}
+library(sf)
+
+createExtent <- function(data_path, shapefile_path) {
+  # Setting the extent of the data to the shape of the shapefile. Requires the
+  # data path and shapefile path as input, and will give the data cropped to the
+  # shape of the shapefile as output.
+  
+  data <- st_read(data_path)
+  extent <- st_read(shapefile_path)
+  
+  #Checking whether the CRS is the same, if not convert the crs of the data to 
+  #the crs of the shapefile (extent)
+  if (st_crs(data) != st_crs(shape)) {
+    message("Transforming CRS to match shapefile CRS")
+    data <- st_transform(data, st_crs(extent))
+  }
+  
+  cropped_data <- st_intersection(data, extent)
+  return(cropped_data)
+}
