f54a8e881781ec5c044041fcf92cfdc5e4ce065a
Drie, Joelle van <joelle.vandrie@wur.nl>
2025-10-07 13:16:16 +0000

    add source main.R

--- main.R
+++ main.R
@@ -6,4 +6,9 @@ if(!dir.exists("tempfiles")){dir.create("tempfiles")}
 if(!dir.exists("data")){dir.create("data")}
 if(!dir.exists("output")){dir.create("output")}
 
-#test
+# Source the download function
+source("downloadFiles.R")
+
+# Download the official timber and oil palm concession data
+download_timber_concessions()
+download_oil_palm_concessions()
