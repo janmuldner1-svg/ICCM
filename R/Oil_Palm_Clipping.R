34a588c23d994a880eadb11e76986b053d990f65
Pascal <pascal.dubbelman@wur.nl>
2025-10-08 16:46:19 +0200

    updated code

--- R/Oil_Palm_Clipping.R
+++ R/Oil_Palm_Clipping.R
@@ -8,14 +8,24 @@ library(terra)
 boundary <- download_and_extract_Kayong()
 download_oil_palm_concessions()
 
-oil_palm_concessions <- vect("data/palm_tree_concessions.json")
+oil_palm_concessions <- st_read("data/palm_tree_concessions.json")
 oil_palm_plantations <- rast("data/ketapang_palm_2023_90.tif")
 
-concessions_clipped <- createExtent(oil_palm_concessions, boundary, output_path = NULL)
-plantations_clipped <- createExtent(oil_palm_plantations, boundary, output_path = NULL)
+sf_use_s2(FALSE)
+
+#concessions_clipped <- createExtent("data/palm_tree_concessions.json","data/Kayong_boundary.geojson")
+concessions_clipped <- st_intersection(oil_palm_concessions, boundary)
+
+spatvector <- vect(boundary)
+
+plantations_clipped <- mask(oil_palm_plantations, spatvector)
 
 # Creating function that identifies mismatch between concessions and plantations
 #function()
 
 mismatches <- mask(plantations_clipped, concessions_clipped, inverse = TRUE)
 plot(mismatches)
+plot(concessions_clipped)
+plot(plantations_clipped)
+
+writeRaster(mismatches,"output/mtest.tif", overwrite = TRUE)
