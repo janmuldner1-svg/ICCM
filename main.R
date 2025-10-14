f6a8c37144a97fecafbc88b5f0edf94b2c7e49d5
Joellev3 <joelle.vandrie@wur.nl>
2025-10-14 15:10:22 +0200

    added open source data for forest, ran it through all steps. lastly added it to the timber mismatch-part for final output

--- main.R
+++ main.R
@@ -12,6 +12,7 @@ if(!dir.exists("output")){dir.create("output")}
 source("R/downloadFiles.R")
 source("R/extent.R")
 source("R/identifyMismatches.R")
+source("R/merge_timber.R")
 
 # Download the official extent of the Kayong Regency (ROI)
 download_and_extract_Kayong()
@@ -20,28 +21,35 @@ extent <- "data/Kayong_boundary.geojson"
 # Download the official timber and oil palm concession data
 download_wood_fiber_concessions()
 wood_fiber_data = "data/wood_fiber_data.json"
+managed_forest_path <- "data/gfw_logging_download_v2020.shp"
 
 download_oil_palm_concessions()
 oil_palm_concessions <- "data/palm_tree_concessions.json"
 
-# Download the oil palm plantations
+# Download open source data for oil palm plantations and forest loss
 oil_palm_plantations <- "data/ketapang_palm_2023_90.tif"
+forest_loss <- rast("data/west_kalimantan_forest_loss_year.tif")
+# Select year 2019-2025 for forest loss
+forest_loss_2019_2024 <- ifel(forest_loss >= 19 & forest_loss <= 24, forest_loss, NA)
 
 # Limit areas of datasets to the extent
-wood_fiber_clipped <- createExtent(wood_fiber_data, extent)
 oil_palm_concessions_clipped <- createExtent(oil_palm_concessions, extent)
 oil_palm_plantations_clipped <- createExtent(oil_palm_plantations, extent)
 
-plot(oil_palm_plantations_clipped)
+wood_fiber_clipped <- createExtent(wood_fiber_data, extent)
+managed_forest_clipped <- createExtent(managed_forest_path, extent)
+forest_loss_clipped <- createExtent(forest_loss_2019_2024, extent)
 
-### OIL PALM ###
+# Combine all wood concession data
+combined_forest_concessions <-merge_timber(managed_forest_clipped, wood_fiber_clipped)
+
+### OIL PALM MISMATCHES ###
 mismatches_oil_palm <- Identify_mismatch_polygons(oil_palm_plantations_clipped, oil_palm_concessions_clipped)
 stats_oil_palm <- Statistics(mismatches_oil_palm)
 
-### TIMBER ###
-
-
-
+### TIMBER MISMATCHES ###
+mismatches_wood <- Identify_mismatch_polygons(forest_loss_clipped, combined_forest_concessions)
+stats_wood <- Statistics(mismatches_wood)
 
 ###
 ###
