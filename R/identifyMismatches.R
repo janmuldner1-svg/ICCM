b3a86b789f31be77a49a443581e88b1566c693d1
Joellev3 <joelle.vandrie@wur.nl>
2025-10-14 21:31:30 +0200

    moved the statistics function to the new file called statistics.R

--- R/identifyMismatches.R
+++ R/identifyMismatches.R
@@ -1,11 +1,4 @@
 # Creating function that clips oil palm concessions and plantations to the extent
-source("R/extent.R")
-source("R/downloadFiles.R")
-
-library(sf)
-library(terra)
-library(dplyr)
-
 
 Identify_mismatch_polygons <- function(commodities_clipped, concessions_clipped){
   # Function that identifies the mismatch polygons with an area equal or above
@@ -32,27 +25,7 @@ Identify_mismatch_polygons <- function(commodities_clipped, concessions_clipped)
   return (filtered_polygons_sf)
 }
 
-Statistics <- function (filtered_polygons_sf){
-  #General statistics, calculates respectively: nr. of polygons, total area (m²), 
-  #mean area (m²), standard deviation of area (m²), and puts this (respectively) 
-  #into a dataframe where m² is converted to km². Requires the data with
-  #filtered polygons as an sf object as input. 
-  n_polygons <- nrow(filtered_polygons_sf)
-  total_area <- sum(filtered_polygons_sf$area_m2)
-  mean_area <- mean(filtered_polygons_sf$area_m2)
-  sd_area <- sd(filtered_polygons_sf$area_m2)
-  n_football_fields <- total_area / 7140
-  
-  stats_df <- data.frame(
-    n_polygons = n_polygons,
-    total_area_km2 = total_area/1000000,
-    mean_area_km2 = mean_area/1000000,
-    sd_area_km2 = sd_area/1000000,
-    n_football_fields = n_football_fields
-  )
-  
-  return (stats_df)
-}
+
 
 
 
