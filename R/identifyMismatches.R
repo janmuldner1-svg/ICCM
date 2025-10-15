ff18f0a8c5e1c2b3128198a0d3a484079ec10f5d
Tosca Koeze <tosca.koeze@wur.nl>
2025-10-15 13:59:03 +0200

    added source for sf_use_s2 line

--- R/identifyMismatches.R
+++ R/identifyMismatches.R
@@ -15,7 +15,7 @@ Identify_mismatch_polygons <- function(commodities_clipped, concessions_clipped)
   #   sf object containing single-part POLYGON geometries of mismatch areas >= 10,000 m²,
   #   with additional 'area_m2' column containing calculated polygon areas
   
-  sf_use_s2(FALSE)
+  sf_use_s2(FALSE) # source used for this line: https://stackoverflow.com/questions/79343873/cropping-in-sf-flat-space-vs-spherical-geometry-and-sf-use-s2
   
   # Identifying mismatches between concessions and commodities
   mismatches <- mask(commodities_clipped, concessions_clipped, inverse = TRUE)
